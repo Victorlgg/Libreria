1bfc682c6f112e58d76029e79d61c9cbfb2e5899
victor <victorlgg97@hotmail.com>
2020-07-26 23:24:00 -0500

    ok

--- back/servicios/libros.php
+++ back/servicios/libros.php
@@ -3,10 +3,11 @@
 //header("Content-Type: application/json; charset=UTF-8");
 
 header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
+header('Access-Control-Allow-Credentials: true');
+header("Access-Control-Allow-Methods: POST, GET");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Content-Type: application/json; charset=UTF-8");
 
 //Conexion a la base de datos
 include_once("conexion.php");
